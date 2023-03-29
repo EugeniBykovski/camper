a3bce733763c25cda574dbbf8a5c4719776d16b7
EugeniBykovski <bykovskieugeni@mail.ru>
2023-03-29 19:15:28 +0200

    added some functionality to the server

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-
-// Route files
-const bootcamps = require('./routes/bootcamps')
+const colors = require('colors')
+const connectDB = require('./config/db')
 
 // load env vars
 dotenv.config({ path: './config/config.env' })
+
+// Connect to DB
+connectDB()
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
 
 const app = express()
 
@@ -20,4 +25,14 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`))
+const server = app.listen(
+  PORT, 
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.yellow.bold)
+)
+
+// Handle unhandled promise rejections (Обработка необработанных отказов обещаний)
+process.on('unhandledRejection', err => {
+  console.log(`Error: ${err.message}`.red)
+  // close server & exit prcess
+  server.close(() => process.exit(1))
+})
